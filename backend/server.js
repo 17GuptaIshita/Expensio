847cd0f7ed31cc068fadce175856145d1f02c6d4
17GuptaIshita <ishitagupta0458@gmail.com>
2025-07-23 11:12:40 +0530

    adding health check for render

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,10 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+app.get("/healthcheck",(req, res) => {
+    res.status(200).json({ message: "Server is healthy" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
